b4c5b8a1ac838c08b7e2a3a916e5cd3886e5df55
kesavan <kesavanplk7@gmail.com>
2023-03-02 19:18:34 +0530

    add small tag for message showing and get elements

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,18 @@
+// get elements
+const userName = document.getElementById("username");
+const email = document.getElementById("email");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirm-password");
+// for testing all the elements are got
+// console.log(userName);
+// console.log(email);
+// console.log(password);
+// console.log(confirmPassword);
 
+//global variables
+
+// functions
+
+// eventListeners
+
+// initial setup
